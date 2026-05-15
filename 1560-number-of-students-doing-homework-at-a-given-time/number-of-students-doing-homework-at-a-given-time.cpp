d9e856df62bba904b86e4480e5a4211a194d4a25
murlicoding <rathi6153@gmail.com>
2026-05-16 01:04:52 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (99.38%) - LeetSync

---
+++ 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int c=0;
+        for(int i=0;i<startTime.size();i++){
+            if(queryTime<=endTime[i] && queryTime>=startTime[i]) c++;
+        }
+        return c;
+    }
+};
