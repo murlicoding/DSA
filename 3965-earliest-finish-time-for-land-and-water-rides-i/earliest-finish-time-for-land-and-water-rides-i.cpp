9a6586fdaa1f2d5fc2fcc0b4073085eb40c11ac7
murlicoding <rathi6153@gmail.com>
2026-06-03 06:35:54 +0530

    Time: 12 ms (31.15%) | Memory: 91.9 MB (26.23%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& a, vector<int>& b, vector<int>& c, vector<int>& d) {
-        int lmin=INT_MAX;
-        int wmin=INT_MAX;
-        for(int i=0;i<a.size();i++){
-            lmin=min(lmin,a[i]+b[i]);
-        }
-        for(int i=0;i<c.size();i++){
-            wmin=min(wmin,c[i]+d[i]);
-        }
+    int earliestFinishTime(
+    vector<int>& a, 
+    vector<int>& b, 
+    vector<int>& c, 
+    vector<int>& d) {
         int ans=INT_MAX;
-        for(int i=0;i<c.size();i++){
-            ans=min(ans,max(lmin,c[i])+d[i]);
-        }
         for(int i=0;i<a.size();i++){
-            ans=min(ans,max(wmin,a[i])+b[i]);
-        }
-        return ans;
+            for(int j=0;j<c.size();j++){
+                 int finish1=max(a[i]+b[i],c[j])+d[j];
+                 int finish2=max(c[j]+d[j],a[i])+b[i];
+                 ans=min(ans,min(finish1,finish2));
+                }
+            }
+    return ans;
     }
 };
