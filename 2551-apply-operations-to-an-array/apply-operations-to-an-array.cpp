7917398414d3aac87d6e766518935391b534e26b
murlicoding <rathi6153@gmail.com>
2026-07-11 17:15:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (61.82%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int i=0;
+        while(i+1<nums.size()){
+            if(nums[i]==nums[i+1]){
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+            
+            i++;
+        }
+         int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+        return nums;
+    }
+};
