ffecc71161bdca7beb21bb126021ccadfbd87839
murlicoding <rathi6153@gmail.com>
2026-07-22 13:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.32%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     string clearDigits(string s) {
-      
         string t="";
-         for (int i = 0; i < s.size(); i++) {
-            if (isdigit(s[i])) {
-                if (!t.empty()) {
-                    t.pop_back();
-                }
-            }
-            else {
-                t.push_back(s[i]);
-            }
+       
+       for(auto x:s){
+        if(isdigit(x)){
+            if(!t.empty())
+            t.pop_back();
         }
-
-        return t;
+        else
+          t.push_back(x);
+          
+       }
+       return t;
     }
 };
