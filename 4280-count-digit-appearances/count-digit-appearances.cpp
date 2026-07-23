4d07ae204a777636d80e60aa7a9380da3227c870
murlicoding <rathi6153@gmail.com>
2026-07-23 21:32:33 +0530

    Time: 3 ms (41.57%) | Memory: 36.9 MB (75.00%) - LeetSync

--- 4280-count-digit-appearances/count-digit-appearances.cpp
+++ 4280-count-digit-appearances/count-digit-appearances.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-   int add(int a,int b){
-    int c=0;
-    while(a>0){
-     if(a%10==b) c++;
-     a/=10;
-    }
-    return c;
-   }
+   
     int countDigitOccurrences(vector<int>& nums, int digit) {
-        int ans=0;
+        int ans=0,c=0;
         for(int i=0;i<nums.size();i++){
-            ans+=add(nums[i],digit);
+            int b=nums[i];
+            while(b>0){
+                if(b%10==digit)c++;
+                b/=10;
+            }
         }
-        return ans;
+        return c;
     }
 };
