9bfcd1d7bf4335f2c5d0e3595da0ce7b6e3a0d55
murlicoding <rathi6153@gmail.com>
2026-07-24 21:19:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (43.26%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.cpp
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     bool areOccurrencesEqual(string s) {
     unordered_map<char,int>mp;
+    int b=0;
     for(auto x:s){
         mp[x]++;
-    }
-    int b=0;
-    for(auto x:mp){
-        b=x.second;
+    
+    
+        b=mp[x];
     }
     for(auto x:mp){
         if(x.second!=b) return false;
