7239dd424e95651bfc4970dc9cedfae54ab351f7
murlicoding <rathi6153@gmail.com>
2026-08-06 09:05:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (5.48%) - LeetSync

---
+++ 2630-alternating-digit-sum/alternating-digit-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        int sum=0;
+        vector<int>ans;
+        while(n>0){
+            ans.push_back(n%10);
+            n/=10;
+        }
+        reverse(ans.begin(),ans.end());
+        for(int i=0;i<ans.size();i++){
+              if (i % 2 == 0)
+                sum += ans[i];
+            else
+                sum -= ans[i];
+        }
+        return sum;
+    }
+};
