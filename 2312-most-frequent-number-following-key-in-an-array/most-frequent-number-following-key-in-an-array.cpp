4a43aa203d9844ef6a5da2f681a4200a68e86368
murlicoding <rathi6153@gmail.com>
2026-08-12 10:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (49.70%) - LeetSync

---
+++ 2312-most-frequent-number-following-key-in-an-array/most-frequent-number-following-key-in-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+        map<int,int>mp;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == key) {
+                mp[nums[i + 1]]++;
+            }
+        }
+        int ans=0;
+        int max=0;
+       for(auto x:mp){
+        if(x.second>max){
+             max = x.second;
+                ans = x.first;
+        }
+       }return ans;
+    }
+};
