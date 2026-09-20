948f2613398371bf354bf6685b28d3e41e950836
MURLI RATHI <rathi6153@gmail.com>
2026-09-20 23:29:15 +0530

    Time: 3 ms (16.14%) | Memory: 9.7 MB (47.88%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            int rev = 26 - (s[i] - 'a');
+            ans += rev * (i + 1);
+        }
+
+        return ans;
+    }
+};
