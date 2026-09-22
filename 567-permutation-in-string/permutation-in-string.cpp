2aec080c06d1e18ac258dfcc08f4873ac9dfe467
MURLI RATHI <rathi6153@gmail.com>
2026-09-22 15:25:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (89.35%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+         vector<int> a(26, 0);
+        vector<int> b(26, 0);
+
+        for(char c : s1)
+            a[c - 'a']++;
+
+        int k = s1.size();
+
+        for(int i = 0; i < s2.size(); i++) {
+            b[s2[i] - 'a']++;
+
+            if(i >= k)
+                b[s2[i-k] - 'a']--;
+
+            if(i >= k-1 && a == b)
+                return true;
+        }
+
+        return false;
+    }
+};
